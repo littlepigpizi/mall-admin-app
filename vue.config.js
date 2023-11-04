a88140d1535fe3f43b548904c487443955bea05d
pipi <13530133736@163.com>
2023-11-04 23:48:26 +0800

    系统初始布局

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,14 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      less: {
+        lessOptions: {
+          javascriptEnabled: true,
+        },
+      },
+    },
+  },
+
 });
